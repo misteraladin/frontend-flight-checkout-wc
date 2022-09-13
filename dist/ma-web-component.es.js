8319fb3ee0ad2f7a7289657261c8610f1764ab51
Wicaksana Pratama <wicaksanapratama@gmail.com>
2022-09-13 18:47:16 +0700

    refactor: fix import register component only from index.ts

--- dist/ma-web-component.es.js
+++ dist/ma-web-component.es.js
@@ -1,5 +1,5 @@
-const i = /* @__PURE__ */ Object.assign({ "./libs/atoms/cards/types.ts": () => import("./types.09fa0212.js"), "./libs/atoms/spinner/index.ts": () => import("./index.7f7eacf2.js"), "./libs/components/header/index.ts": () => import("./index.7759fe03.js"), "./libs/components/localization-host/index.ts": () => import("./index.74cacba2.js"), "./libs/views/booking-confirmation/index.ts": () => import("./index.0ca07e04.js"), "./libs/views/booking-confirmation/lang.ts": () => import("./lang.df582137.js"), "./libs/views/booking-detail/index.ts": () => import("./index.05706599.js"), "./libs/views/booking-detail/lang.ts": () => import("./lang.22238f48.js") });
-for (const s in i)
-  i[s]().then((o) => {
-    o.register();
+const i = /* @__PURE__ */ Object.assign({ "./libs/atoms/spinner/index.ts": () => import("./index.7f7eacf2.js"), "./libs/components/header/index.ts": () => import("./index.7759fe03.js"), "./libs/components/localization-host/index.ts": () => import("./index.74cacba2.js"), "./libs/views/booking-confirmation/index.ts": () => import("./index.515a60d0.js"), "./libs/views/booking-detail/index.ts": () => import("./index.0990bb06.js") });
+for (const o in i)
+  i[o]().then((s) => {
+    s.register();
   });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // import './styles/index.scss';
 
 // app.mount('#app');
-const modules = import.meta.glob('./libs/**/*.ts');
+const modules = import.meta.glob('./libs/**/index.ts');
 for (const path in modules) {
   modules[path]().then((mod) => {
     (mod as { register: () => void }).register();
