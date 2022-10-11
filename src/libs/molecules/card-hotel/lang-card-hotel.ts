7bf8abb1fe45827b1e6c0ac5ae7f8b0f2f1134b6
Iqbal Hamdani <miqbalh94@gmail.com>
2022-10-11 20:50:21 +0700

    [Card Hotel]

---
+++ src/libs/molecules/card-hotel/lang-card-hotel.ts
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    room_remain: '{total} Rooms left',
+    room_empty: 'Kamar Tidak Tersedia',
+  },
+  id: {
+    room_remain: 'Sisa {total} Kamar',
+    room_empty: 'Kamar Tidak Tersedia',
+  },
+};
