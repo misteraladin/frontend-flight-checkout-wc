705dd182bfe54b3df4f217c70b25513219c6dfc0
Faris Imaduddin <faris.imaduddin@misteraladin.com>
2022-10-27 13:18:40 +0700

    delete unused files

--- src/libs/atoms/tab/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineCustomElement } from 'vue';
-import Tab from './tab.ce.vue';
-
-const tab = defineCustomElement(Tab);
-
-export function register() {
-  customElements.define('ma-tab', tab);
-}
