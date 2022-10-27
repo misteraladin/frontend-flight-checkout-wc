3165796db1861ceaca62c3bd754f0f1311cca339
Faris Imaduddin <faris.imaduddin@misteraladin.com>
2022-10-27 16:54:12 +0700

    card promo

--- data/index.js
+++ data/index.js
@@ -1,5 +1,6 @@
 import buttonGroupHomepage from "./button-group-homepage.json";
 import cardHotel from "./card-hotel.json";
+import promos from "./promos.json";
 import sectionHotelFlashsale from "./section-hotel-flashsale.json";
 import sectionMallFlashsale from "./section-mall-flashsale.json";
 import sectionHotelSelected from "./section-hotel-selected.json";
@@ -8,6 +9,7 @@ import sectionMallSelected from "./section-mall-selected.json";
 export default {
   buttonGroupHomepage,
   cardHotel,
+  promos,
   sectionHotelFlashsale,
   sectionMallFlashsale,
   sectionHotelSelected,
---
+++ src/libs/molecules/card-promo/card-promo.ce.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="ma-card-promo"
+    :class="{ 'is-small': isSmall }"
+    @click="handleClick"
+  >
+    <img :alt="data.description" :src="data.image_url" />
+    <div class="ma-card-promo__text-wrapper">
+      <label>
+        {{ data.title }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { RootObject as IRootObject } from "./type-card-promo";
+
+const props = defineProps({
+  data: {
+    type: String,
+    required: true,
+  },
+
+  isSmall: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const data: IRootObject = reactive(props.data ? JSON.parse(props.data) : null);
+
+const emit = defineEmits(['click']);
+const handleClick = (event: MouseEvent) => {
+  emit('click', event);
+
+  console.log('should open new tab to detailed promo');
+  
+};
+
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/molecules/card-promo";
+</style>
