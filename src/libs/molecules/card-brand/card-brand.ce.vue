c453ca31a9fbfd9ee755e910fa5e6a8f41236a9e
Iqbal Hamdani <miqbalh94@gmail.com>
2022-11-07 05:19:30 +0700

    [Card Brand] Update structure

---
+++ src/libs/molecules/card-brand/card-brand.ce.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+
+const props = defineProps({
+  data: {
+    type: String,
+    required: true,
+  },
+
+  small: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+interface IBrand {
+  id: number;
+  sequence: number;
+  logo: string;
+  name: string;
+  url: string;
+}
+
+const brand: IBrand = reactive(props.data ? JSON.parse(props.data) : null);
+</script>
+
+<template>
+  <a :href="brand.url" class="brand-item">
+    <div class="brand-image-wrapper">
+      <img :src="brand.logo" :alt="brand.name" />
+    </div>
+
+    <div class="brand-name">
+      {{ brand.name }}
+    </div>
+  </a>
+</template>
+
+<style lang="scss">
+@use "@/styles/molecules/card-brand";
+</style>
